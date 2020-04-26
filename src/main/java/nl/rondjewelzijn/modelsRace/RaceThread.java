25de1dd6efd18b2e7455a20471d781dd955cc2bd
Farley <fguldenaar@tuta.io>
2020-04-26 21:11:33 +0200

    Edited threadnumber

--- src/main/java/nl/rondjewelzijn/modelsRace/RaceThread.java
+++ src/main/java/nl/rondjewelzijn/modelsRace/RaceThread.java
@@ -11,11 +11,11 @@ public class RaceThread implements IThread {
     @Override
     public void threads() {
 
-        for (int runnableNumber = 1; runnableNumber <=3; runnableNumber++) {
+        for (int runnableNumber = 1; runnableNumber <=5; runnableNumber++) {
             Runnable r = new RaceCounter();
 
             //Create number of threads
-            for (int threadNumber = 1; threadNumber < 3; threadNumber++) {
+            for (int threadNumber = 1; threadNumber <=5; threadNumber++) {
                 Thread t = new Thread(r);
                 System.out.println("Started: " + t.getName());
                 t.start();
